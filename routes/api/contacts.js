5ec0361b790fe9d153397b2aca2d15a68b473e8d
Dmytro Vasylenko <tredstoun651@uk.net>
2021-06-16 13:00:21 +0300

    test commit

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -6,7 +6,7 @@ router.get('/', async (req, res, next) => {
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  res.json({ message: 'new message' })
 })
 
 router.post('/', async (req, res, next) => {

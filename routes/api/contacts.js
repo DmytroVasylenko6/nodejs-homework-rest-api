9a6dde813a205dacb9bfbc9a79938a7647863104
Dmytro Vasylenko <tredstoun651@uk.net>
2021-06-20 16:15:05 +0300

    final

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,24 +1,52 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
+const contactsAction = require('../../model/index');
+const { addAndUpdateContactMiddleware } = require('../../middlewares/validationMiddlewares');
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+
+//GET ContactList
+router.get('/', async (req, res) => {
+  const contacts = await contactsAction.listContacts()
+  res.status(200).json({ contacts: contacts, status: 'success' })
 })
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'new message' })
+//GET Contact by ID
+router.get('/:contactId', async (req, res) => {
+  const contactId = req.params.contactId;
+  const contact = await contactsAction.getContactById(contactId);
+
+  if (contact) {
+    res.status(200).json({ contact: contact, status: 'success' });
+  } else {
+    res.status(404).json({ message: 'Not found' });
+  }
 })
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+//POST Add Contact
+router.post('/', addAndUpdateContactMiddleware, async (req, res) => {
+
+  const contactAdd = await contactsAction.addContact(req.body);
+  res.status(200).json({ contact: contactAdd, status: 'success' });
 })
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+//DELETE Delete contact
+router.delete('/:contactId', async (req, res) => {
+  const contactId = req.params.contactId;
+  const contact = await contactsAction.removeContact(contactId);
+  console.log(contact)
+  if (contact) {
+    res.status(200).json({  message: `contact "${contact.name}" is deleted`, status: 'success' });
+  } else {
+     res.status(404).json({ message: 'Not found' });
+  }
 })
 
-router.patch('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+//PUT Update contact
+router.put('/:contactId', addAndUpdateContactMiddleware, async (req, res) => {
+
+  const contactId = req.params.contactId;
+  const contactUpdate = await contactsAction.updateContact(contactId, req.body);
+  res.status(200).json({ updatedContact: contactUpdate, status: 'success' });
 })
 
 module.exports = router
